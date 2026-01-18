c76211e8e2cb7c1db8fb4ac39650e0662e3fc77f
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-01-18 21:01:46 +0530

    disabled snacks dashboard

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.lazyvim_picker = "telescope"
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins (must be first)
@@ -32,7 +34,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "rose-pine", "habamax" } },
+  install = { colorscheme = { "rose-pine" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
