23b6b4dcc1b1403a16b958f44fe66a36bcf90c75
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-04-17 14:53:17 +0530

    add: lume colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -244,4 +244,13 @@ return {
             transparent_background = false,
         },
     },
+    {
+      "danfry1/lume",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require("lume").setup()
+        vim.cmd("colorscheme lume")
+      end,
+    },
 }
