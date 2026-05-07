e130f0324444c6b19379dfbb42fd7573e21e0ea5
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-05-07 15:08:59 +0530

    python update

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -22,7 +22,7 @@ return {
             },
           },
         },
-        ruff_lsp = {
+        ruff = {
           init_options = {
             settings = {
               args = {},
@@ -71,7 +71,7 @@ return {
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
         "pyright",
-        "ruff-lsp",
+        "ruff",
         "black",
         "isort",
         "pylint",
