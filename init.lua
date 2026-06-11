730667ed8a75988a252abdee833d0f03ea32075b
rainz <ronakkamboj26@gmail.com>
2026-06-11 19:39:58 +0530

    add scrolloff=8

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+vim.opt.scrolloff = 8
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
